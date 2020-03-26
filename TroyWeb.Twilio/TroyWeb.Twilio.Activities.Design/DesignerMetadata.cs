b618bb47c866d279e2bcc557573fb66fc032b30c
Joe Iaquinto <joseph.iaquinto@troyweb.com>
2020-03-26 12:33:29 -0400

    Add project files.

---
+++ TroyWeb.Twilio/TroyWeb.Twilio.Activities.Design/DesignerMetadata.cs
@@ -0,0 +1,27 @@
+﻿using System.Activities.Presentation.Metadata;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using TroyWeb.Twilio.Activities.Design.Designers;
+using TroyWeb.Twilio.Activities.Design.Properties;
+
+namespace TroyWeb.Twilio.Activities.Design
+{
+    public class DesignerMetadata : IRegisterMetadata
+    {
+        public void Register()
+        {
+            #region Setup
+
+            var builder = new AttributeTableBuilder();
+            builder.ValidateTable();
+
+            var categoryAttribute = new CategoryAttribute($"{Resources.Category}");
+
+            #endregion Setup
+
+
+
+            MetadataStore.AddAttributeTable(builder.CreateTable());
+        }
+    }
+}
